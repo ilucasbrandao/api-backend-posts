95b70251438b731f27fd554688873c94b58c72e6
Lucas De Sousa Brandao <lucas.brandao@dass.local>
2025-08-29 16:23:46 -0300

    banco integrado com o front end

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 import express from "express";
 import dotenv from "dotenv";
 import pool from "./db.js";
+import cors from "cors";
 
 dotenv.config();
-
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 const port = process.env.PORT || 3000;
 
-// ------ ROTA GET ------ //
+// ------ ROTA GET LIST ------ //
 app.get("/posts", async (req, res) => {
   try {
     const posts = await pool.query("SELECT * FROM posts");
@@ -19,8 +20,24 @@ app.get("/posts", async (req, res) => {
   }
 });
 
+// ------ ROTA GET ONE ------ //
+app.get("/posts/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const post = await pool.query("SELECT * FROM posts WHERE id = $1", [id]);
+
+    if (post.rowCount === 0) {
+      return res.status(404).json({ message: "Post não encontrado!" });
+    }
+
+    res.status(200).json(post.rows[0]);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
 // ------ ROTA POST ------ //
-app.post("/posts", async (req, res) => {
+app.post("/posts/create", async (req, res) => {
   try {
     let { title, author, dataPostagem } = req.body;
 
@@ -32,9 +49,7 @@ app.post("/posts", async (req, res) => {
       "INSERT INTO posts (title, author, dataPostagem) VALUES ($1, $2, $3) RETURNING id",
       [title, author, dataPostagem]
     );
-    res
-      .status(201)
-      .json({ message: "Post criado com sucesso!", id: newPost.rows[0].id });
+    res.status(201).json(newPost.rows[0]);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
@@ -52,11 +67,10 @@ app.put("/posts/:id", async (req, res) => {
       [title, author, dataPostagem, id]
     );
 
-    console.log(updateQuery.rows);
     if (updateQuery.rowCount === 0) {
       return res.status(404).json({ message: "Post não encontrado!" });
     }
-    res.status(200).json({ message: "Post atualizado com sucesso!" });
+    res.status(200).json(updateQuery.rowCount[0]);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
@@ -75,7 +89,7 @@ app.delete("/posts/:id", async (req, res) => {
     if (deleteQuery.rowCount === 0) {
       return res.status(404).json({ message: "Post não encontrado!" });
     }
-    res.status(200).json({ message: "Post deletado com sucesso!" });
+    res.status(200).json({ id: id });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
