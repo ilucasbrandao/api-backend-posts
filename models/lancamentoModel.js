a1c11b21a9386935a05baf46b531977e9f85e7fb
Lucas De Sousa Brandao <lucas.brandao@dass.local>
2025-09-01 17:03:06 -0300

    atualização do model 'getById'

--- models/lancamentoModel.js
+++ models/lancamentoModel.js
@@ -4,3 +4,10 @@ export const getLancamentos = async (table) => {
   const result = await pool.query(`SELECT * FROM ${table}`);
   return result.rows;
 };
+
+export const getById = async (table, id) => {
+  const result = await pool.query(`SELECT * FROM ${table} WHERE ${id} = $1`, [
+    id,
+  ]);
+  return result.rows[0];
+};
