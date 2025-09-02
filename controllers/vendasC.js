ccbdcaf734b291e9e402b9c6d4370e1495f2526a
Lucas De Sousa Brandao <lucas.brandao@dass.local>
2025-09-02 16:49:47 -0300

    rota de update implementada

--- controllers/vendasC.js
+++ controllers/vendasC.js
@@ -2,6 +2,7 @@ import * as Model from "../models/vendasM.js";
 
 const table = "vendas";
 
+// ?------ LISTAR TODOS ------?
 export const getVendasAll = async (req, res) => {
   try {
     const vendas = await Model.getVendasAll(table);
@@ -11,6 +12,7 @@ export const getVendasAll = async (req, res) => {
   }
 };
 
+// ?------ LISTAR INDIVIDUAL ------?
 export const getVendasById = async (req, res) => {
   try {
     const { id } = req.params;
@@ -24,11 +26,13 @@ export const getVendasById = async (req, res) => {
   }
 };
 
+// ?------ CRIAR ------?
 export const createVenda = async (req, res) => {
   try {
     const { dateSale, productName, amount, seller, totalValue } = req.body;
     if (!dateSale || !productName || !amount || !seller || !totalValue)
       res.status(400).json({ message: "Campos faltando!" });
+
     const saleCompleted = await Model.createVenda(
       table,
       ["dateSale", "productName", "amount", "seller", "totalValue"],
@@ -36,6 +40,27 @@ export const createVenda = async (req, res) => {
     );
     console.log(saleCompleted);
     res.status(201).json(saleCompleted);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// ?------ ATUALIZAR ------?
+export const updateVenda = async (req, res) => {
+  try {
+    const { newDateSale, newProductName, newAmount, newSeller, newTotalValue } =
+      req.body;
+    const { id } = req.params;
+    if (!id) {
+      return res.status(404).json({ message: error.message });
+    }
+    const updateSale = await Model.updateVenda(
+      table,
+      ["dateSale", "productName", "amount", "seller", "totalValue"],
+      [newDateSale, newProductName, newAmount, newSeller, newTotalValue]
+    );
+    console.log(updateSale);
+    res.status(200).json(updateSale);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
--- models/vendasM.js
+++ models/vendasM.js
@@ -1,18 +1,18 @@
 import { pool } from "../db.js";
 
-// --------- ROTA LISTAR ---------//
+// !--------- ROTA LISTAR ---------! //
 export const getVendasAll = async (table) => {
   const result = await pool.query(`SELECT * FROM ${table}`);
   return result.rows;
 };
 
-// --------- ROTA LISTAR POR ID ---------//
+// !--------- ROTA LISTAR POR ID ---------! //
 export const getVendasById = async (table, id) => {
   const result = await pool.query(`SELECT * FROM ${table} WHERE id = $1`, [id]);
   return result.rows[0];
 };
 
-// --------- ROTA POST ---------//
+// !--------- ROTA POST ---------! //
 export const createVenda = async (table, columns, values) => {
   const cols = columns.join(", "); // nome das colunas separadas por ", "
   const placeholders = values.map((_, i) => `$${i + 1}`).join(", "); // enumera cada array da lista passada em $1, $2, $3 ...
@@ -21,4 +21,22 @@ export const createVenda = async (table, columns, values) => {
     values
   );
   return result.rows[0];
+};
+
+// !--------- ROTA PUT ---------! //
+export const updateVenda = async (table, columns, values, id) => {
+  // Exemplo: columns = ["title", "author"]
+  // values = ["Novo título", "Novo autor"]
+
+  const setQuery = columns
+    .map((col, i) => `${col} = COALESCE($${i + 1}, ${col})`)
+    .join(", ");
+
+  const query = `UPDATE ${table} SET ${setQuery} WHERE id = $${
+    columns.length + 1
+  } RETURNING *`;
+
+  const result = await pool.query(query, [...values, id]);
+
+  return result.rows[0]; // retorna o registro atualizado
 };
